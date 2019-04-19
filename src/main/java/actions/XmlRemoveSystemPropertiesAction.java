17bae062efde9c8c82ec568e033c57130ae20988
Dominik Briner <dominik.briner@students.unibe.ch>
2019-04-19 12:53:37 +0200

    Fix AWT error message by using ApplicationManager.getApplication().invokeLater() (Which means that the deletion has to be wrapped in a WriteCommandAction)

--- src/main/java/actions/XmlRemoveSystemPropertiesAction.java
+++ src/main/java/actions/XmlRemoveSystemPropertiesAction.java
@@ -1,6 +1,8 @@
 package actions;
 
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -24,6 +26,7 @@ public class XmlRemoveSystemPropertiesAction extends BaseIntentionAction {
     private static final Logger LOG = Logger.getInstance(XmlRemoveSystemPropertiesAction.class);
     private final String message = "Remove system properties...";
     private String familyName = "Mgnl Utils";
+    private Project project;
 
 
     @NotNull
@@ -50,31 +53,38 @@ public class XmlRemoveSystemPropertiesAction extends BaseIntentionAction {
      */
     @Override
     public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
-        ArrayList<XmlTag> propertyTags = new ArrayList<>();
-        // Sort list system properties ignoring case
-        Set<String> systemPropertyNames = new TreeSet<>(String::compareToIgnoreCase);
+        this.project = project;
+        ApplicationManager.getApplication().invokeLater(
+                () -> {
+                    ArrayList<XmlTag> propertyTags = new ArrayList<>();
+                    // Sort list system properties ignoring case
+                    Set<String> systemPropertyNames = new TreeSet<>(String::compareToIgnoreCase);
 
-        collectSystemPropertyTags(file, propertyTags, systemPropertyNames);
-        ChooseValuesDialog choosePropertiesDialog = new ChooseValuesDialog(systemPropertyNames, project, false, "remove-xml-system-property");
-        choosePropertiesDialog.pack();
+                    collectSystemPropertyTags(file, propertyTags, systemPropertyNames);
+                    ChooseValuesDialog choosePropertiesDialog = new ChooseValuesDialog(systemPropertyNames, project, false, "remove-xml-system-property");
+                    choosePropertiesDialog.pack();
 
-        // Return iff dialog was cancelled
-        if (!choosePropertiesDialog.showAndGet())
-            return;
+                    // Return iff dialog was cancelled
+                    if (!choosePropertiesDialog.showAndGet())
+                        return;
 
-        ArrayList<String> chosenProperties = choosePropertiesDialog.getChosenValues();
 
-        removeChosenTags(chosenProperties, propertyTags);
+                    ArrayList<String> chosenProperties = choosePropertiesDialog.getChosenValues();
+
+                    removeChosenTags(chosenProperties, propertyTags);
+                });
     }
 
     /**
      * Puts all the system properties' tags of the file into propertyTags and the corresponding "sv:name" attribute
      * value into systemPropertyNames.
-     * @param file PsiFile to be queried for system properties
-     * @param propertyTags Collection to put the property Tags in there
+     *
+     * @param file                PsiFile to be queried for system properties
+     * @param propertyTags        Collection to put the property Tags in there
      * @param systemPropertyNames Collection to put the system properties in
      */
-    private void collectSystemPropertyTags(PsiFile file, Collection<XmlTag> propertyTags, Collection<String> systemPropertyNames) {
+    private void collectSystemPropertyTags(PsiFile
+                                                   file, Collection<XmlTag> propertyTags, Collection<String> systemPropertyNames) {
         PsiRecursiveElementWalkingVisitor fileVisitor = new PsiRecursiveElementWalkingVisitor() {
             @Override
             public void visitElement(final PsiElement element) {
@@ -101,28 +111,31 @@ public class XmlRemoveSystemPropertiesAction extends BaseIntentionAction {
         LOG.debug("Looking for system properties in " + file.getName() + "...");
         fileVisitor.visitFile(file);
         LOG.debug(propertyTags.size() + " tags with one of " + systemPropertyNames.size() + " different "
-        + "system properties found in file " + file + ".");
+                + "system properties found in file " + file + ".");
     }
 
     /**
      * Removes the XmlTags whose "sv:name" attribute value was chosen
+     *
      * @param chosenProperties The attribute values of "sv:name" to be deleted
-     * @param propertyTags The XmlTags which may be deleted
+     * @param propertyTags     The XmlTags which may be deleted
      */
     private void removeChosenTags(ArrayList<String> chosenProperties, ArrayList<XmlTag> propertyTags) {
-        for (XmlTag propTag:propertyTags) {
-            try {
-                if (chosenProperties.contains(Objects.requireNonNull(propTag.getAttribute("sv:name")).getValue()))
-                    propTag.delete();
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            for (XmlTag propTag : propertyTags) {
+                try {
+                    if (chosenProperties.contains(Objects.requireNonNull(propTag.getAttribute("sv:name")).getValue()))
+                        propTag.delete();
+                }
+                // Deletion not possible
+                catch (IncorrectOperationException exc) {
+                    LOG.info("Could not delete XML-tag " + propTag + " containing system property!", exc);
+                }
+                // Attribute is null
+                catch (NullPointerException npe) {
+                    LOG.info("Attribute \'sv:name\' of XML-tag " + propTag + " has no value.", npe);
+                }
             }
-            // Deletion not possible
-            catch (IncorrectOperationException exc) {
-                LOG.info("Could not delete XML-tag " + propTag + " containing system property!", exc);
-            }
-            // Attribute is null
-            catch (NullPointerException npe) {
-                LOG.info("Attribute \'sv:name\' of XML-tag " + propTag + " has no value.", npe);
-            }
-        }
+        });
     }
 }
