b0e4520fce97f39a5fea3b09dd8ecbf68f0cd2cb
Dominik Briner <dominik.briner@students.unibe.ch>
2019-05-09 18:19:19 +0200

    Bugfix: Gets rid of NPEs thrown in FileUtil#isFileType. They probably appeared when documents weren't loaded or an unkown filetype was encountered.

--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -1,11 +1,15 @@
 package util;
 
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class FileUtil {
 
-    public static boolean isFileType(PsiFile file, @NotNull String extension) {
-        return file.getFileType().getDefaultExtension().toLowerCase().equals(extension.toLowerCase()) || extension.toLowerCase().equals(file.getVirtualFile().getExtension().toLowerCase());
+    public static boolean isFileType(@Nullable PsiFile file,@Nullable String extension) {
+        try {
+            return file.getFileType().getDefaultExtension().toLowerCase().equals(extension.toLowerCase()) || extension.toLowerCase().equals(file.getVirtualFile().getExtension().toLowerCase());
+        } catch(NullPointerException npe) {
+            return false;
+        }
     }
 }
